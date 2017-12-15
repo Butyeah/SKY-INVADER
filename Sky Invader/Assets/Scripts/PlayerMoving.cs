f3637f9169f51fae2c0f0685e1896d4d0afce390
Butyeah <teemu.nokkanen99@gmail.com>
2017-12-15 10:59:44 +0200

    Luke Skywalkerin hahmo petettiin
    Päivitys:
    Pelaajan liike

--- Sky Invader/Assets/Scripts/PlayerMoving.cs
+++ Sky Invader/Assets/Scripts/PlayerMoving.cs
@@ -14,7 +14,20 @@ public class PlayerMoving : MonoBehaviour {
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
 		}
 
-		//Jatka TÄSTÄ
+		if (Input.GetKey (KeyCode.A)) {
+
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+		//Käännetään pelihahmo transformin avulla
+		if (GetComponent<Rigidbody2D> ().velocity.x > 0) {
+
+			//Suunta on oikea
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+		} else if (GetComponent<Rigidbody2D> ().velocity.x < 0) {
+
+			//suunta on vasemmalle
+			transform.localScale = new Vector3 (-1f, 1f, 1f);
+		}
 		
 	}
 }
