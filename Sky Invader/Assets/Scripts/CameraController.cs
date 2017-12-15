52f8c0cdd85e811a042b98d5433b7a5908d1b253
Tumeee <valkamo.tuomas@gmail.com>
2017-12-15 11:02:39 +0200

    Kameran liike
    CameraController scriptin luonti. Kamera liikkuu hitaasti ylöspäin.

---
+++ Sky Invader/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float tileSpeedZ;
+
+	private Vector3 startPosition;
+
+// Use this for initialization
+void Start () {
+		startPosition = transform.position;
+}
+
+// Update is called once per frame
+void Update () {
+
+	// Käännetään vihollinen
+	//transform.localScale = new Vector3(1f, 1f, 1f);
+	//GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+	float newPosition = Mathf.Repeat (Time.time * moveSpeed, tileSpeedZ);
+	transform.position = startPosition + Vector3.up * newPosition;
+}
+}
